47f19589bf8c17eb956fa37d4a45411e6a25a47c
TATUHIKO <TATUHIKO@192.168.1.9>
2023-01-25 17:46:52 +0900

    レッスン3税計算修正

--- src/Review01.java
+++ src/Review01.java
@@ -6,7 +6,7 @@ public class Review01 {
         int price = 4000;
         //消費税額の計算
         int taxamount = taxcalculation(price);
-        //計算結果を表示
+        //元の金額、税込み金額、税額の表示
         System.out.println(price + "円 の商品の税込金額は" +  (price + taxamount) + "円 " + "（消費税額は" + taxamount + "円） です。");
     }
 
@@ -14,7 +14,7 @@ public class Review01 {
     public static int taxcalculation(int taxamount) {
         //税率の変数と税率の設定、税率の変更が必要ならここで設定する。
         double taxrate = 0.2d;
-        //元の金額と消費税金額を足す　（int）で小数点以下切り捨て
+        //消費税金額の計算　（int）で小数点以下切り捨て
         int result =(int)(taxrate*taxamount);
         //計算結果を戻す
         return result;
